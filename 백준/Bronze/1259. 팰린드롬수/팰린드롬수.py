e6c9e2691e7167cf993d0f56822a33fa3af22383
SangHyeok0908 <126949574+SangHyeok0908@users.noreply.github.com>
2024-02-23 17:00:54 +0900

    [Bronze I] Title: 팰린드롬수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,19 +1,10 @@
 import sys
 
 while True:
-    reverseNumber = []
-    answer = True
-    number = list(sys.stdin.readline().rstrip())
-    if number[0] == '0':
+    number = sys.stdin.readline().rstrip()
+    if number == '0':
         break
-
-    for i in reversed(range(len(number))):
-        reverseNumber.append(number[i])
-
-    for i in range(len(number)):
-        if reverseNumber[i] != number[i]:
-            answer = False
-            print("no")
-            break
-    if answer:
+    elif number == number[::-1]:
         print("yes")
+    else:
+        print("no")
