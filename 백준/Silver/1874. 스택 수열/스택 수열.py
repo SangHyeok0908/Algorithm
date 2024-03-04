89904be285e0c62c084d50d2c3bf0989f0d5fdae
SangHyeok0908 <126949574+SangHyeok0908@users.noreply.github.com>
2024-03-04 13:58:24 +0900

    [Silver II] Title: 스택 수열, Time: 196 ms, Memory: 32704 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -3,24 +3,23 @@ import sys
 N = int(sys.stdin.readline())
 operand_list = []
 opcode_list = []
-rank_list = [i for i in range(1, N + 1)]
-rank_list.sort(reverse= True)
 isPossible = True
+i = 1
 
 for _ in range(N):
     temp = int(sys.stdin.readline())
 
-    if operand_list and operand_list[-1] > temp:
-        isPossible = False
+    if isPossible:
+        while i <= temp:
+            operand_list.append(i)
+            opcode_list.append('+')
+            i += 1
 
-    while rank_list:
-        if temp < rank_list[-1]:
-            break
-        operand_list.append(rank_list.pop())
-        opcode_list.append('+')
-
-    operand_list.pop()
-    opcode_list.append('-')
+        if operand_list[-1] == temp:
+            operand_list.pop()
+            opcode_list.append('-')
+        elif operand_list[-1] > temp:
+            isPossible = False
 
 if isPossible:
     for o in opcode_list:
