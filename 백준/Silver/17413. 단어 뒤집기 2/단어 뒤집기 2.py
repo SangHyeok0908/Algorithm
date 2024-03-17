e3a9a73d7a1cb72938a7fe91df54428bd7e3ebef
SangHyeok0908 <126949574+SangHyeok0908@users.noreply.github.com>
2024-03-17 23:16:25 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 308 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,33 +1,31 @@
 import sys
-from collections import deque
 
-input_string = deque(sys.stdin.readline().rstrip())
-reverse_string = deque()
+input_string = list(sys.stdin.readline().rstrip())
+reverse_string = []
 output_string = ''
 is_reverse = True
 
-while input_string:
-    character = input_string.popleft()
-    if character == '<':
+for string in input_string:
+    if string == '<':
         while reverse_string:
             output_string += reverse_string.pop()
         is_reverse = False
-        output_string += character
+        output_string += string
         continue
-    elif character == '>':
+    elif string == '>':
         is_reverse = True
-        output_string += character
+        output_string += string
         continue
-    elif character == ' ' and is_reverse:
+    elif string == ' ' and is_reverse:
         while reverse_string:
             output_string += reverse_string.pop()
-        output_string += character
+        output_string += string
         continue
 
     if is_reverse:
-        reverse_string.append(character)
+        reverse_string.append(string)
     else:
-        output_string += character
+        output_string += string
 
 while reverse_string:
     output_string += reverse_string.pop()
