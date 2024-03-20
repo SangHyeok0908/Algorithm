934fdd46f446a814c56e3876f9d69ad8e87aecd7
SangHyeok0908 <126949574+SangHyeok0908@users.noreply.github.com>
2024-03-20 18:52:34 +0900

    [Gold V] Title: 회문, Time: 420 ms, Memory: 35700 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -1,46 +1,39 @@
 import sys
 
+def function(string_list, len_string):
+    count = 0
+    for i in range(len_string // 2):
+        if string_list[i] == string_list[len_string - 1 - i]:
+            count += 1
+    if count == len_string // 2:
+        return True
+    return False
+
 T = int(sys.stdin.readline())
 for _ in range(T):
     input_string = list(sys.stdin.readline().rstrip())
     length = len(input_string)
+    is_print = False
 
-    if length == 1:
+    for i in range(length):
+        if input_string[i] == input_string[length - 1 - i]:
+            continue
+        is_print = True
+        start = i
+        end = length - 1 - i
+        deleted_front_list = input_string[start + 1:end + 1]
+        deleted_back_list = input_string[start:end]
+        length = len(deleted_front_list)
+
+        if function(deleted_front_list, length):
+            print(1)
+            break
+        elif function(deleted_back_list, length):
+            print(1)
+            break
+        else:
+            print(2)
+            break       
+         
+    if not is_print:
         print(0)
-        continue
-    else:
-        is_print = False
-        for i in range(length):
-            if input_string[i] != input_string[length - 1 - i]:
-                start = i
-                end = length - 1 - i
-                delete_front_list = input_string[start + 1:end + 1]
-                delete_back_list = input_string[start:end]
-                length_delete = len(delete_front_list)
-                count_front = 0
-                count_back = 0
-
-                if length_delete < 2:
-                    print(1)
-                    is_print = True
-                    break
-
-                for j in range(length_delete // 2):
-                    if delete_front_list[j] == delete_front_list[length_delete - 1 - j]:
-                        count_front += 1
-                
-                for j in range(length_delete // 2):
-                    if delete_back_list[j] == delete_back_list[length_delete - 1 - j]:
-                        count_back += 1
-
-                if count_front == length_delete // 2 or count_back == length_delete // 2:
-                    print(1)
-                    is_print = True
-                    break
-                else:
-                    print(2)
-                    is_print = True
-                    break
-                
-        if not is_print:
-            print(0)
