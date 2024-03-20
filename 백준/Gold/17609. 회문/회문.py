021cde25b34080b493511a96ad6bf43867d800f9
SangHyeok0908 <126949574+SangHyeok0908@users.noreply.github.com>
2024-03-20 18:55:36 +0900

    [Gold V] Title: 회문, Time: 320 ms, Memory: 35700 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -1,11 +1,7 @@
 import sys
 
-def function(string_list, len_string):
-    count = 0
-    for i in range(len_string // 2):
-        if string_list[i] == string_list[len_string - 1 - i]:
-            count += 1
-    if count == len_string // 2:
+def function(string_list):
+    if string_list == string_list[::-1]:
         return True
     return False
 
@@ -23,17 +19,16 @@ for _ in range(T):
         end = length - 1 - i
         deleted_front_list = input_string[start + 1:end + 1]
         deleted_back_list = input_string[start:end]
-        length = len(deleted_front_list)
 
-        if function(deleted_front_list, length):
+        if function(deleted_front_list):
             print(1)
             break
-        elif function(deleted_back_list, length):
+        elif function(deleted_back_list):
             print(1)
             break
         else:
             print(2)
             break       
-         
+
     if not is_print:
         print(0)
