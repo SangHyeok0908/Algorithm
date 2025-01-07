c5f23e3ec967516d6d64d59e8801a653bf917ac7
Seo SangHyeok <126949574+SangHyeok0908@users.noreply.github.com>
2025-01-07 15:58:00 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 648 ms, Memory: 295524 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        LinkedList<Integer> list = new LinkedList<>();
+        LinkedList<Integer> result = new LinkedList<>();
+
+        String[] str = br.readLine().split(" ");
+        int N = Integer.parseInt(str[0]);
+        int K = Integer.parseInt(str[1]);
+
+        for (int i = 0; i < N; i++) {
+            list.add(i + 1);
+        }
+
+        while (!list.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                list.add(list.pop());
+            }
+
+            result.add(list.pop());
+        }
+
+        int size = result.size();
+//        bw.write("<");
+        System.out.print("<");
+        for(int i = 0; i < size - 1; i++) {
+            System.out.print(result.pop() + ", ");
+        }
+        System.out.println(result.pop() + ">");
+//        bw.write(">" + "\n");
+//        bw.close();
+    }
+}
