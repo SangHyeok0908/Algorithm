2a6dc15d815590dd3e95993cd09cd4fb8ed5f81c
Seo SangHyeok <126949574+SangHyeok0908@users.noreply.github.com>
2025-01-16 15:18:08 +0900

    [Silver I] Title: 그림, Time: 444 ms, Memory: 59472 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -5,10 +5,12 @@ public class Main {
     static int[][] graph;
     static int n, m;
     static boolean[][] isVisited;
+    static int dfsCount;
+    static int[][] position = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
 
     static int bfs(int x, int y) {
         int result = 1;
-        int[][] position = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         Queue<int[]> queue = new LinkedList<>();
 
         queue.add(new int[]{x, y});
@@ -33,13 +35,26 @@ public class Main {
         return result;
     }
 
+    static void dfs(int x, int y) {
+        isVisited[x][y] = true;
+        dfsCount++;
+
+        for (int[] pos : position) {
+            int currentX = x + pos[0];
+            int currentY = y + pos[1];
+
+            if (currentX >= n || currentY >= m || currentX < 0 || currentY < 0) continue;
+
+            if (graph[currentX][currentY] == 1 && !isVisited[currentX][currentY]) dfs(currentX, currentY);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] input = br.readLine().split(" ");
         List<Integer> answer = new ArrayList<>();
-        boolean isZero = true;
 
         n = Integer.parseInt(input[0]);
         m = Integer.parseInt(input[1]);
@@ -48,29 +63,32 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             graph[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
 
-            if (isZero) {
-                for(int j =0; j < graph[i].length; j++) {
-                    if (graph[i][j] == 1) {
-                        isZero = false;
-                        break;
-                    }
+        // bfs
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                if (!isVisited[i][j] && graph[i][j] == 1) {
+//                    answer.add(bfs(i, j));
+//                }
+//            }
+//        }
+
+        // dfs
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dfsCount = 0;
+                if (!isVisited[i][j] && graph[i][j] == 1) {
+                    dfs(i, j);
+                    answer.add(dfsCount);
                 }
             }
         }
 
-        if (isZero) {
+        if (answer.isEmpty()) {
             bw.write("0\n0\n");
             bw.close();
             return;
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!isVisited[i][j] && graph[i][j] == 1) {
-                    answer.add(bfs(i, j));
-                }
-            }
         }
 
         Collections.sort(answer);
