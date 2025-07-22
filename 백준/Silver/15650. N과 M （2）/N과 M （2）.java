ccb8982de76b93cf560df488fe48b5a3d941850c
Seo SangHyeok <126949574+SangHyeok0908@users.noreply.github.com>
2025-07-22 15:42:33 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,36 +1,36 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
+
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N, M;
-    static int[] arr;
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int N, M;
+  static int[] arr;
 
-    public static void main(String[] args) throws IOException {
-        String[] s = br.readLine().split(" ");
-        N = Integer.parseInt(s[0]);
-        M = Integer.parseInt(s[1]);
-        arr = new int[M];
+  public static void main(String[] args) throws Exception {
+    String[] input = br.readLine().split(" ");
+    
+    N = Integer.parseInt(input[0]);
+    M = Integer.parseInt(input[1]);
+    arr = new int[M];
 
-        recursion(0, 0);
-        br.close();
-        bw.close();
+    solution(0, 1);
+  }
+
+  static public void solution(int k, int v) {
+    if (k == M) {
+      for(int a : arr) {
+        System.out.print(a + " ");
+      }
+      System.out.println();
+      return;
     }
 
-    static void recursion(int depth, int startIdx) throws IOException {
-        if (depth == M) {
-            for(int i : arr) {
-                bw.write((i + 1) + " ");
-            }
-            bw.write("\n");
-            return;
-        }
-
-        for(int i = startIdx; i < N; i++) {
-            arr[depth] = i;
-            recursion(depth + 1, i + 1);
-        }
+    for(int i = v; i <= N; i++) {
+      arr[k] = i;
+      solution(k + 1, i + 1);
     }
+  }
 }
