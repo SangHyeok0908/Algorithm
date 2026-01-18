4a0afe71039f3e3d4a08cd1376970f846c43e284
Seo SangHyeok <126949574+SangHyeok0908@users.noreply.github.com>
2026-01-18 15:19:07 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -1,116 +1,107 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
   static int N, M;
   static char[][] matrix;
 
   public static void main(String[] args) throws Exception {
-    int[] red = null, blue = null;
-    int[] input = Arrays.stream(br.readLine().split(" "))
-        .mapToInt(Integer::parseInt).toArray();
-    N = input[0];
-    M = input[1];
+    int[] redIdx = null;
+    int[] blueIdx = null;
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
     matrix = new char[N][M];
 
     for (int i = 0; i < N; i++) {
-      matrix[i] = br.readLine().toCharArray();
+      st = new StringTokenizer(br.readLine());
+      matrix[i] = st.nextToken().toCharArray();
 
       for (int j = 0; j < M; j++) {
         if (matrix[i][j] == 'R') {
-          red = new int[] { i, j };
+          redIdx = new int[] { i, j };
         } else if (matrix[i][j] == 'B') {
-          blue = new int[] { i, j };
+          blueIdx = new int[] { i, j };
         }
       }
     }
 
-    int answer = bfs(red, blue);
-    System.out.println(answer);
+    int result = move(redIdx, blueIdx);
+    System.out.println(result);
   }
 
-  static int bfs(int[] initRed, int[] initBlue) {
-    int[] dy = new int[] { -1, 1, 0, 0 };
-    int[] dx = new int[] { 0, 0, -1, 1 };
+  static int move(int[] redIdx, int[] blueIdx) {
     Queue<int[]> queue = new ArrayDeque<>();
     boolean[][][][] isVisited = new boolean[N][M][N][M];
+    int[] dy = new int[] { -1, 1, 0, 0 };
+    int[] dx = new int[] { 0, 0, -1, 1 };
 
-    queue.add(new int[] { initRed[0], initRed[1], initBlue[0], initBlue[1], 0 });
-    isVisited[initRed[0]][initRed[1]][initBlue[0]][initBlue[1]] = true;
+    queue.add(new int[] { redIdx[0], redIdx[1], blueIdx[0], blueIdx[1], 0 });
 
     while (!queue.isEmpty()) {
       int[] poll = queue.poll();
 
       for (int i = 0; i < 4; i++) {
-        int curRY = poll[0];
-        int curRX = poll[1];
-        int curBY = poll[2];
-        int curBX = poll[3];
-        int cnt = poll[4];
-        boolean isRedGoal = false;
-        boolean isBlueGoal = false;
+        int redY = poll[0];
+        int redX = poll[1];
+        int blueY = poll[2];
+        int blueX = poll[3];
+        int result = poll[4];
+        boolean redGoal = false;
+        boolean blueGoal = false;
 
-        if (cnt >= 10) {
+        if (result >= 10) {
           return -1;
         }
 
-        // 벽을 만나기 전까지 이동하기
-        while (matrix[curRY + dy[i]][curRX + dx[i]] != '#') {
-          curRY += dy[i];
-          curRX += dx[i];
+        // red ball 움직이기
+        while (matrix[redY + dy[i]][redX + dx[i]] != '#') {
+          redY += dy[i];
+          redX += dx[i];
 
-          // Red가 구멍을 만났다면 종료
-          if (matrix[curRY][curRX] == 'O') {
-            isRedGoal = true;
+          if (matrix[redY][redX] == 'O') {
+            redGoal = true;
             break;
           }
         }
 
-        while (matrix[curBY + dy[i]][curBX + dx[i]] != '#') {
-          curBY += dy[i];
-          curBX += dx[i];
+        // blue ball 움직이기
+        while (matrix[blueY + dy[i]][blueX + dx[i]] != '#') {
+          blueY += dy[i];
+          blueX += dx[i];
 
-          // Blue가 구멍을 만났다면 continue
-          if (matrix[curBY][curBX] == 'O') {
-            isBlueGoal = true;
+          if (matrix[blueY][blueX] == 'O') {
+            blueGoal = true;
             break;
           }
         }
 
-        if (isBlueGoal) {
+        if (redGoal && !blueGoal) {
+          return result + 1;
+        } else if (blueGoal) {
           continue;
-        } else if (isRedGoal) {
-          return cnt + 1;
         }
 
-        // 이동한 후 Red와 Blue의 위치가 동일하다면
-        if (curRY == curBY && curRX == curBX) {
+        if (redY == blueY && redX == blueX) {
+          int diffRed = Math.abs(redY - poll[0] + redX - poll[1]);
+          int diffBlue = Math.abs(blueY - poll[2] + blueX - poll[3]);
 
-          // 이동 거리를 비교해서 더 큰 이동거리를 가진 공을 한 칸 뒤로 이동시키기
-          int rChanged = Math.abs(curRY - poll[0]) + Math.abs(curRX - poll[1]);
-          int bChanged = Math.abs(curBY - poll[2]) + Math.abs(curBX - poll[3]);
-
-          if (rChanged > bChanged) {
-            curRY -= dy[i];
-            curRX -= dx[i];
+          if (diffRed > diffBlue) {
+            redY -= dy[i];
+            redX -= dx[i];
           } else {
-            curBY -= dy[i];
-            curBX -= dx[i];
+            blueY -= dy[i];
+            blueX -= dx[i];
           }
         }
 
-        if (!isVisited[curRY][curRX][curBY][curBX]) {
-          isVisited[curRY][curRX][curBY][curBX] = true;
-          queue.add(new int[] { curRY, curRX, curBY, curBX, cnt + 1 });
+        if (isVisited[redY][redX][blueY][blueX])
+          continue;
 
-          // System.out.println(
-          // String.format("red(%d, %d), blue(%d, %d), cnt = %d",
-          // curRY, curRX, curBY, curBX, cnt + 1));
-        }
+        isVisited[redY][redX][blueY][blueX] = true;
+        queue.add(new int[] { redY, redX, blueY, blueX, result + 1 });
       }
     }
 
