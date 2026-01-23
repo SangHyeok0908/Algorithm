908c2ed6ef76ad26807ca0f13c4947ba6f663d61
Seo SangHyeok <126949574+SangHyeok0908@users.noreply.github.com>
2026-01-23 09:31:18 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 58.54 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,76 +1,72 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    Queue<int[]> jobQueue = new ArrayDeque<>();
-    PriorityQueue<int[]> waitQueue = new PriorityQueue<>((a, b) -> {
-       if (a[2] < b[2]) {
-                return -1;
-            } else if (a[2] > b[2]) {
-                return 1;
-            }
-            
-            if (a[1] < b[1]) {
-                return -1;
-            } else if (a[1] > b[1]) {
-                return 1;
-            }
-            
-            if (a[0] < b[0])
-                return -1; 
-            return 1;
-    });
     
     public int solution(int[][] jobs) {
-        int answer = 0;     
+        // {idx, requestTime, jobTime}
+        Queue<int[]> initQ = new PriorityQueue<>((a, b) -> {
+           if (a[1] < b[1]) {
+               return -1;
+           } else if (a[1] > b[1]) {
+               return 1;
+           } 
+           
+           if (a[2] < b[2]) {
+               return -1;
+           } else if (a[2] > b[2]) {
+               return 1;
+           }
+           
+           return a[0] - b[0];
+       });
+        Queue<int[]> waitQ = new PriorityQueue<>((a, b) -> {
+           if (a[2] < b[2]) {
+               return -1;
+           } else if (a[2] > b[2]) {
+               return 1;
+           } 
+           
+           if (a[1] < b[1]) {
+               return -1;
+           } else if (a[1] > b[1]) {
+               return 1;
+           }
+           
+           return a[0] - b[0];
+       });
         
-        Arrays.sort(jobs, (a, b) -> {
-            if (a[0] < b[0]) {
-                return -1;
-            } else if (a[0] > b[0]) {
-                return 1;
-            }
-            
-            if (a[1] < b[1]) {
-                return -1;
-            } else if (a[1] > b[1]) {
-                return 1;
-            }
-            
-            return -1;
-        });
-
         for (int i = 0; i < jobs.length; i++) {
-            jobQueue.add(new int[]{i, jobs[i][0], jobs[i][1]});   
+            initQ.add(new int[]{i, jobs[i][0], jobs[i][1]});
         }
         
-        // for (int[] i : jobQueue) {
-        //     System.out.println(i[0] + " " + i[1] + " " + i[2]);
-        // }
-
-        int[] first = jobQueue.poll();
-        int time = first[1];
-        waitQueue.add(first);
-        
-        while(!jobQueue.isEmpty() || !waitQueue.isEmpty()) {
-            int[] poll = waitQueue.poll();
-            
-            time += poll[2];
-            answer += time - poll[1];
-            
-            // System.out.println(String.format("time = %d, poll = %d [%d, %d], answer = %d", 
-            //                                  time, poll[0], poll[1], poll[2], answer));
-            
-            while(!jobQueue.isEmpty() && jobQueue.peek()[1] <= time) {
-                waitQueue.add(jobQueue.poll());
+        int curTime = 0;
+        int timeSum = 0;
+        while(!initQ.isEmpty() || !waitQ.isEmpty()) {
+            // 아직 initQ에 작업이 있지만, 요청시간이 curTime보다 클 때
+            if (!initQ.isEmpty() && curTime < initQ.peek()[1] && waitQ.isEmpty()) {
+                curTime = initQ.peek()[1];   
             }
             
-            if (waitQueue.isEmpty() && !jobQueue.isEmpty()) {
-                int[] nextPoll = jobQueue.poll();
-                time = nextPoll[1];
-                waitQueue.add(nextPoll);
+            // initQ에서 waitQ로 job 넣기
+            while (!initQ.isEmpty() && initQ.peek()[1] <= curTime) {
+                System.out.printf("initQ poll, curTime = %d\n", curTime);
+                waitQ.add(initQ.poll());
+            }
+            
+            // waitQ에서 작업할 job 빼기
+            if (!waitQ.isEmpty()) {
+                int[] curJob = waitQ.poll();
+                System.out.printf("waitQ poll, curTime = %d, idx = %d\n", curTime, curJob[0]);
+                
+                // job 수행
+                System.out.println("job processing");
+                System.out.printf("before curTime = %d\n", curTime);
+                curTime += curJob[2];
+                timeSum += curTime - curJob[1];
+                System.out.printf("after curTime = %d\n", curTime);
             }
         }
-        
-        return answer / jobs.length;
+        return timeSum / jobs.length;
     }
 }
