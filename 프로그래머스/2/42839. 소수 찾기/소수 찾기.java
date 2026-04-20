670c6c41e18d652bad28cc83b36dacdd4c21d6bd
Seo SangHyeok <126949574+SangHyeok0908@users.noreply.github.com>
2026-04-20 23:28:03 +0900

    [level 2] Title: 소수 찾기, Time: 89.83 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,74 +1,77 @@
 import java.util.*;
 
 class Solution {
-    String[] nums;
-    String[] dp;
-    boolean[] isVisited;
-    Set<Integer> set = new HashSet<>();
     
+    int answer = 0;
+    int n;
+    int[] original;
+    boolean[] visited;
+    int[] arr;
+    Set<Integer> set = new HashSet<>();
+   
     public int solution(String numbers) {
-        nums = new String[numbers.length()];
+        n = numbers.length();
+        original = new int[n];
         
-        for (int i = 1; i <= numbers.length(); i++) {
-            nums[i - 1] = numbers.substring(i - 1, i);
+        for (int i = 1; i <= n; i++) {
+            original[i - 1] = Integer.parseInt(numbers.substring(i - 1, i));
         }
-        
-        for (int i = 1; i <= numbers.length(); i++) {
-            dp = new String[i];
-            isVisited = new boolean[numbers.length()];
-            recursion(0, i);
-        }
-        
-        // System.out.println("set 출력");
-        // for (Integer i : set) {
-        //     System.out.println(i);
-        // }
-        // System.out.println("set 출력 end");
-        
-        int cnt = 0;
-        for (Integer i : set) {
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    cnt++;
-                    // System.out.println("제외 수 = " + i);
-                    break;
-                }
+                
+        for (int i = 0; i < n; i++) {
+            if (!set.contains(original[i]) && isShosu(original[i])) {
+                answer++;
+                set.add(original[i]);
             }
         }
         
-        return set.size() - cnt;
+        for (int size = 2; size <= n; size++) {
+            arr = new int[size];
+            visited = new boolean[n];
+            dfs(0, size);
+        }
+        
+        return answer;
     }
     
-    void recursion(int depth, int digit) {
-        if (depth == digit) {
-            String temp = "";
+    void dfs(int depth, int size) {
+        if (depth == size) {
+            int num = 0;
             
-            for (String r : dp) {
-                temp += r;
+            for (int i = 0; i < size; i++) {
+                // System.out.print(arr[i] + " ");
+                num = num * 10 + arr[i];
             }
             
-            int result = Integer.parseInt(temp);
+            // System.out.println(num);
             
-            // System.out.printf("조합된 수 = %d\n", result);
-            
-            for (int i = 2; i < result; i++) {
-                if (result % i == 0) return;
+            if (!set.contains(num) && isShosu(num)) {
+                answer++;
+                set.add(num);
             }
-            
-            if (result >= 2)
-                set.add(result);
             return;
         }
         
-        for (int i = 0; i < nums.length; i++) {
-            // System.out.printf("depth = %d, i = %d\n", depth, i);
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue;
+            }
             
-            if ((depth == 0 && nums[i].equals("0")) || isVisited[i]) continue;
+            visited[i] = true;
+            arr[depth] = original[i];
             
-            dp[depth] = nums[i];
-            isVisited[i] = true;
-            recursion(depth + 1, digit);
-            isVisited[i] = false;
+            dfs(depth + 1, size);
+            visited[i] = false;
         }
+    }
+    
+    boolean isShosu(int num) {
+        if (num <= 1) 
+            return false;
+        
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) 
+                return false;
+        }
+        return true;
     }
 }
