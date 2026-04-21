8d11a3b95e74aa4c4a7ccbf656c80b84cedaf93f
Seo SangHyeok <126949574+SangHyeok0908@users.noreply.github.com>
2026-04-21 09:51:34 +0900

    [level 2] Title: 피로도, Time: 4.01 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,61 +2,31 @@ import java.util.*;
 
 class Solution {
     
-    int n, k;
+    int n;
     boolean[] visited;
-    int[] arr;
     int answer = 0;
     int[][] dungeons;
     
     public int solution(int k, int[][] dungeons) {
-        this.k = k;
         this.dungeons = dungeons;
         n = dungeons.length;
         visited = new boolean[n];
-        arr = new int[n];
         
-        dfs(0);
+        dfs(k, 0);
         return answer;
     }
     
-    void dfs(int depth) {
-        if (depth == n) {
-            // for (int i = 0; i < n; i++) {
-            //     System.out.print(arr[i] + " ");
-            // }
-            // System.out.println();
-            answer = Math.max(answer, search());
-            return;
-        }
-        
+    void dfs(int cur, int cnt) {
         for (int i = 0; i < n; i++) {
-            if (visited[i]) {
+            if (visited[i] || dungeons[i][0] > cur) {
                 continue;
             }
             
             visited[i] = true;
-            arr[depth] = i;
-            dfs(depth + 1);
-            
+            dfs(cur - dungeons[i][1], cnt + 1);
             visited[i] = false;
         }
-    }
-    
-    int search() {
-        int cur = k;
-        int cnt = 0;
         
-        // System.out.println("======");
-        for (int i = 0; i < n; i++) {
-            int idx = arr[i];
-            
-            if (cur >= dungeons[idx][0]) {
-                cur -= dungeons[idx][1];
-                cnt++;
-                
-                // System.out.println(dungeons[idx][0] + " " + dungeons[idx][1]);
-            }
-        }
-        return cnt;
+        answer = Math.max(answer, cnt);
     }
 }
