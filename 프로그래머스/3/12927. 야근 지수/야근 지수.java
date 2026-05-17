c67d24d93e5ccdc89ab17ea535f8871d91aa809b
Seo SangHyeok <126949574+SangHyeok0908@users.noreply.github.com>
2026-05-17 17:37:22 +0900

    [level 3] Title: 야근 지수, Time: 0.70 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,28 +3,66 @@ import java.util.*;
 class Solution {
     
     public long solution(int n, int[] works) {
-        Queue<Integer> queue = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
+        int left = 0, right = 0, maxMid = 0;
+        long total = 0;
+        
         for (int w : works) {
-            queue.add(w);
+            right = Math.max(right, w);
+            maxMid = Math.max(maxMid, w);
+            total += w;
+        }
+        if (total <= n) {
+            return 0;
         }
         
-        while(n > 0) {
-            int poll = queue.poll();
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            int diff = 0;
             
-            if (poll == 0) {
-                break;
+            for (int w : works) {
+                if (w > mid) {
+                    diff += w - mid;
+                }
             }
             
-            queue.add(poll - 1);
-            n--;
+            if (diff <= n) {
+                maxMid = Math.min(maxMid, mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
         
-        long answer = 0;
-        while(!queue.isEmpty()) {
-            int poll = queue.poll();
-            // System.out.println(poll);
-            answer += Math.pow(poll, 2);
+        // System.out.println("maxMid = " + maxMid);
+        
+        int copyN = n;
+        for (int i = 0; i < works.length; i++) {
+            if (maxMid < works[i]) {
+                copyN -= (works[i] - maxMid);
+                works[i] = maxMid;
+            }
         }
+        
+        // int idx = 0;
+        // while(copyN > 0) {
+        //     works[idx]--;
+        //     copyN--;
+        //     idx = (idx + 1) % works.length;
+        // }
+        
+        long answer = 0;
+        for (int i = 0; i < works.length; i++) {
+            if (copyN > 0 && works[i] == maxMid) {
+                works[i]--;
+                copyN--;
+            }
+            answer += (long) works[i] * works[i];
+        }
+        
+        // long answer = 0;
+        // for (int w : works) {
+        //     answer += Math.pow(w, 2);
+        // }
         return answer;
     }
 }
